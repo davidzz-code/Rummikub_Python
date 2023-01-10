5a0a5c5f16d39efe277ac5f3f11e5c1f6ba1b438
Sergio Majada <smajadamanresa@cifpfbmoll.eu>
2023-01-10 16:27:36 +0100

    refactor: manual merge from main to main_prueba

--- main_prueba.py
+++ main_prueba.py
@@ -1,18 +1,38 @@
-import pygame, sys
+import pygame, sys, os
 
 pygame.init()
 
-#################### PANTALLA ####################
+# Tamaño de pantalla
 screen_size = (1000, 665)
 screen = pygame.display.set_mode(screen_size)
 
-# Crea una fuente para escribir texto en la pantalla
-font = pygame.font.Font(None, 70)
+# Paleta de colores
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+magenta = (255, 0, 255)
+soft_blue = (133, 193, 233)
 
+# Variables
+FPS = 25
+clock = (
+    pygame.time.Clock()
+)  # Creación de una variable "clock" para ajustar la velocidad de las imágenes
+bg = pygame.image.load(
+    os.path.join("Python", "Rummikub_Python", "resources", "bg.jpg")
+)  # fondo del juego
+
+# Crea una fuente para escribir texto en la pantalla
+font = pygame.font.SysFont("Helvetica", 70)
 run = True
 
-def main_menu(): # Menu principal
-    pygame.display.set_caption('Menu')
+
+def main_menu():  # Menu principal
+    pygame.display.set_caption("Rummikub")  # Título de la ventana
 
     while run:
         # Crea el mensaje que quieras dibujar por pantalla
@@ -21,7 +41,7 @@ def main_menu(): # Menu principal
         screen.blit(text_font, (100, 100))
 
         # Crea un botón que servirá para comenzar el juego
-        button_font = pygame.font.Font(None, 50)
+        button_font = pygame.font.SysFont("arial", 50)
         button_text = button_font.render("Comenzar a jugar ", 1, (80, 100, 255))
         button_rect = button_text.get_rect()
         button_rect.center = (600, 300)
@@ -32,33 +52,33 @@ def main_menu(): # Menu principal
         # Actualiza la pantalla para mostrar los cambios
         pygame.display.flip()
 
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if button_rect.collidepoint(event.pos):
-                        play()
-
-def play(): # Pantalla de juego
-
-    rectangle = pygame.rect.Rect(200, 134, 65, 80)
-    rectangle_draging = False
-    WHITE   = (255,255,255)
-    FPS = 144
-    clock = pygame.time.Clock()
-    bg = pygame.image.load("bg.jpg")
-
-
-    while run:    
+        # bucle principal para salir de la pantalla
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
+            # Acceder al juego tras pulsar el botón
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_rect.collidepoint(event.pos):
+                    play()
+
+
+def play():  # Pantalla de juego
+    # Creación de un rectángulo
+    rectangle = pygame.rect.Rect(200, 134, 65, 80)
+    rectangle_draging = False
+
+    # Bucle principal
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            ####### Inicio condiciones Dragging #######
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:            
+                if event.button == 1:
                     if rectangle.collidepoint(event.pos):
                         rectangle_draging = True
                         mouse_x, mouse_y = event.pos
@@ -66,7 +86,7 @@ def play(): # Pantalla de juego
                         offset_y = rectangle.y - mouse_y
 
             elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:            
+                if event.button == 1:
                     rectangle_draging = False
 
             elif event.type == pygame.MOUSEMOTION:
@@ -74,12 +94,12 @@ def play(): # Pantalla de juego
                     mouse_x, mouse_y = event.pos
                     rectangle.x = mouse_x + offset_x
                     rectangle.y = mouse_y + offset_y
-    
+            ####### Final condiciones Dragging #######
+
             screen.blit(bg, (0, 0))
-            pygame.draw.rect(screen, WHITE, rectangle)
+            pygame.draw.rect(screen, white, rectangle)
             pygame.display.flip()
             clock.tick(FPS)
 
 
 main_menu()
-
