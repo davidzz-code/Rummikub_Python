9a16b75d30ded236f03e67642a3143aae0c7a681
Sergio Majada <smajadamanresa@cifpfbmoll.eu>
2023-01-13 20:12:36 +0100

    Creación funciones fichas y random

--- RummyGame.py
+++ RummyGame.py
@@ -1,3 +1,4 @@
+import random
 
 def conjuntoFichas():
     fichas = []
@@ -5,11 +6,20 @@ def conjuntoFichas():
     colores = ["Naranja", "Azul", "Negro", "Rojo"]
     valores = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
-    for color in colores:
-        for valor in valores:
-            fichaVal = "{} {}".format(color, valor)
-            fichas.append(fichaVal)
+    for z in range(2):
+        for color in colores:
+            for valor in valores:
+                fichaVal = "{} {}".format(color, valor)
+                fichas.append(fichaVal)
+    return fichas
+
+def shuffleFichas(fichas):
+    for fichasPos in range(len(fichas)):
+        randPos = random.randint(0, 51)
+        fichas[fichasPos], fichas[randPos] = fichas[randPos], fichas[fichasPos]
     print(fichas)
     return fichas
 
-conjuntoFichas()
+fichasRummy = conjuntoFichas()
+fichasRummy = shuffleFichas(fichasRummy)
+
