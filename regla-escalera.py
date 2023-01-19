dc45e54781e9a011ce657b473eb5804d803ecc84
David Ramírez <rrdavid08@gmail.com>
2023-01-19 16:25:39 +0100

    Añadido SORT para escalera y doc. reglas juntas

--- regla-escalera.py
+++ regla-escalera.py
@@ -32,19 +32,27 @@ for i in tablero[conjunto - 1]:
 # Valora si es posible colocar la ficha en una escalera
 correcto = False
 while correcto == False:
-    if colorFichaUsuario != colorConjunto or numeroFichaUsuario != (numeroConjunto + 1):
+    if colorFichaUsuario != colorConjunto or numeroFichaUsuario != (listaNumerosTablero[-1] + 1) and  numeroFichaUsuario != (listaNumerosTablero[0] - 1):
         print("Movimiento incorrecto, vuelve a probar.")
         ficha = input("¿Qué ficha quieres usar?: ")
         conjunto = int(input("¿En qué conjunto la quieres colocar?: "))
 
         colorFichaUsuario = ficha.split(" ")
         numeroFichaUsuario = int(colorFichaUsuario.pop(1))
-        
+
         for i in tablero[conjunto - 1]:
             colorConjunto = i.split(" ")
             numeroConjunto = int(colorConjunto.pop(1))
             listaColoresTablero.append(colorConjunto[0])
             listaNumerosTablero.append(numeroConjunto)
+
+    elif numeroFichaUsuario == (listaNumerosTablero[0] - 1):
+        correcto = True
+        listaNumerosTablero.append(numeroFichaUsuario)
+        listaNumerosTablero.sort()
+        tablero[conjunto - 1].insert(0, ficha)
+        tableroFunction(tablero)
+        
     else:
         correcto = True
         tablero[conjunto - 1].append(ficha)
